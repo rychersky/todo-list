0aea397bc2443b0bee7c22ee41034785f829aa86
Ryan Chernosky <rychersky@gmail.com>
2021-03-01 19:54:10 -0600

    buildTaskList & buildEditTaskForm refactored
    Stripped all hardcoded HTML out of index.html.

--- index.js
+++ index.js
@@ -7,28 +7,21 @@ class Task {
   }
 }
 
-// DOM Constants
-
-const createNewTask = document.querySelector('#createNewTask');
-const newTaskForm = document.querySelector('#newTaskForm');
-const title = document.querySelector(`input[name="title"]`);
-const description = document.querySelector(`input[name="description"]`);
-const dueDate = document.querySelector(`input[name="dueDate"`);
-const priority = document.querySelector(`select[name="priority"]`);
-const addTask = document.querySelector('#addTask');
-const cancelNewTask = document.querySelector('#cancelNewTask');
-const taskList = document.querySelector('#taskList');
-const editTaskForm = document.querySelector('#editTaskForm');
-
+const main = document.querySelector('main');
 const localStorage = [
-  new Task('buy some shoes', `let's go to the mall you guys`, Date.now(), 'low'),
-  new Task('plant a carrot', 'solve world hunger', Date.now(), 'medium'),
+  new Task('buy some shoes', `let's go to the mall you guys`, new Date(), 'low'),
+  new Task('plant a carrot', 'solve world hunger', new Date(), 'medium'),
 ];
 
-// Functions
-
 function buildTaskList() {
-  taskList.innerHTML = '';
+  // New Task button
+  main.innerHTML = /*html*/`
+    <button type="button" id="createNewTask">New Task</button>
+  `;
+  const newTaskButton = main.querySelector('#createNewTask');
+  newTaskButton.addEventListener('click', buildNewTaskForm);
+
+  // Create each task item in UI
   localStorage.forEach(task => {
     const newElement = document.createElement('div');
     newElement.innerHTML = /*html*/`
@@ -37,78 +30,94 @@ function buildTaskList() {
       <button type="button" class="cancelTask">X</button>
     `;
     const editTaskButton = newElement.querySelector('button.editTaskItem');
-    editTaskButton.addEventListener('click', buildEditTaskForm.bind(null, task));
+    editTaskButton.addEventListener('click', () => buildEditTaskForm(task));
     newElement.classList.add('taskItem');
-    taskList.appendChild(newElement);
+    main.appendChild(newElement);
   });
 }
 
 function buildEditTaskForm(task) {
-  taskList.classList.add('hidden');
-  createNewTask.classList.add('hidden');
-  editTaskForm.classList.remove('hidden');
-  editTaskForm.innerHTML = /*html*/`
-    <label for="title">Title:</label><br>
-    <input type="text" name="title" value="${task.title}"><br>
+  // Format task date for HTML
+  const taskYear = task.dueDate.getUTCFullYear().toString();
+  const taskMonth = (task.dueDate.getUTCMonth() + 1).toString().padStart(2, '0');
+  const taskDay = task.dueDate.getUTCDate().toString().padStart(2, '0');
+  const taskDate = `${taskYear}-${taskMonth}-${taskDay}`;
+  
+  // Build edit form
+  main.innerHTML = /*html*/`
+    <form id="editTaskForm">
+      <label for="title">Title:</label><br>
+      <input type="text" name="title" value="${task.title}"><br>
 
-    <label for="description">Description:</label><br>
-    <input type="text" name="description" value="${task.description}"><br>
+      <label for="description">Description:</label><br>
+      <input type="text" name="description" value="${task.description}"><br>
 
-    <label for="dueDate">Due Date:</label><br>
-    <input type="date" name="dueDate"><br>
+      <label for="dueDate">Due Date:</label><br>
+      <input type="date" name="dueDate" value="${taskDate}"><br>
 
-    <label for="priority">Priority:</label><br>
-    <select name="priority">
-      <option value="high">High</option>
-      <option value="medium">Medium</option>
-      <option value="low" selected>Low</option>
-    </select><br><br>
+      <label for="priority">Priority:</label><br>
+      <select name="priority">
+        <option value="high" 
+          ${task.priority === 'high' ? 'selected' : ''}>High</option>
+        <option value="medium" 
+          ${task.priority  === 'medium' ? 'selected' : ''}>Medium</option>
+        <option value="low" 
+          ${task.priority === 'low' ? 'selected' : ''}>Low</option>
+      </select><br><br>
 
-    <button type="button" id="saveTask">Save</button>
-    <button type="button" id="cancelEditTask">Cancel</button>
+      <button type="button" id="saveTask">Save</button>
+      <button type="button" id="cancelEditTask">Cancel</button>
+    </form>
   `;
-  const saveTask = editTaskForm.querySelector('#saveTask');
+
+  // Save button
+  const saveTask = main.querySelector('#saveTask');
   saveTask.addEventListener('click', () => {
-    
+    const newTitle = main.querySelector('input[name="title"]').value;
+    const newDesc = main.querySelector('input[name="description"]').value;
+    const newDueDate = main.querySelector('input[name="dueDate"]').valueAsDate;
+    const newPriority = main.querySelector('select[name="priority"]').value;
+    if (newTitle && newDesc && newDueDate && newPriority) {
+      task.title = newTitle;
+      task.description = newDesc;
+      task.dueDate = newDueDate;
+      task.priority = newPriority;
+      buildTaskList();
+    } else {
+      alert("Please fill out all fields");
+    }
   });
+
+  // Cancel button
+  const cancelEditTask = main.querySelector('#cancelEditTask');
+  cancelEditTask.addEventListener('click', buildTaskList);
 }
 
+function buildNewTaskForm() {
+  main.innerHTML = /*html*/`
+    <form id="newTaskForm">
+      <label for="title">Title:</label><br>
+      <input type="text" name="title"><br>
 
-function exitForm() {
-  createNewTask.classList.remove('hidden');
-  newTaskForm.classList.add('hidden');
-  taskList.classList.remove('hidden');
-  resetFormFields();
+      <label for="description">Description:</label><br>
+      <input type="text" name="description"><br>
+
+      <label for="dueDate">Due Date:</label><br>
+      <input type="date" name="dueDate"><br>
+
+      <label for="priority">Priority:</label><br>
+      <select name="priority">
+        <option value="high">High</option>
+        <option value="medium">Medium</option>
+        <option value="low" selected>Low</option>
+      </select><br><br>
+
+      <button type="button" id="addTask">Save</button>
+      <button type="button" id="cancelNewTask">Cancel</button>
+    </form>
+  `;
 }
 
-function resetFormFields() {
-  title.value = '';
-  description.value = '';
-  dueDate.value = null;
-  priority.value = 'low';
-}
 
 buildTaskList();
 
-// Event Listeners
-
-createNewTask.addEventListener('click', () => {
-  createNewTask.classList.add('hidden');
-  newTaskForm.classList.remove('hidden');
-  taskList.classList.add('hidden');
-});
-
-addTask.addEventListener('click', () => {
-  if (title.value && description.value && dueDate.value) {
-    const task = new Task(
-      title.value, description.value, dueDate.valueAsDate, priority.value
-    );
-    localStorage.push(task);
-    buildtaskList();
-    exitForm();
-  } else {
-    alert("Please fill out all fields.");
-  }
-});
-
-cancelNewTask.addEventListener('click', exitForm);
